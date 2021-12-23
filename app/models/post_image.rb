87e15cfb8bc4dddfffb4e5819e5ceec2844d5aee
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-12-23 00:00:00 +0000

    [finish] 4-3

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,8 @@
 class PostImage < ApplicationRecord
 
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  
   attachment :image #_idは含めずに記述する
   #モデルに、画像アップ用のメソッド(attachment)を追加して、フィールド名に(image)を指定する
   #refileを使用する上でのルール
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   #resourcesメソッドはルーティングを一括して自動生成してくれる
   #onlyは生成するルーティングを限定するオプション
   #不要なルーティングがされないようにonlyを設定する
+    resources:post_comments, only: [:create, :destroy]
+    #コメントは投稿画像に対してコメントされる。→ post_commentsは、post_imagesに結びつく
+    #このような親子関係を　ネストする　という
+  end
+
 end
